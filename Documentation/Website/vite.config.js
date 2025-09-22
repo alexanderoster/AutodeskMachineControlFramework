bc705f98705aa2c69d6e82c062a7fd057cd75cb1
Alexander Oster <alexanderoster@users.noreply.github.com>
2025-09-23 01:17:23 +0200

    Fixed vite config

--- Documentation/Website/vite.config.js
+++ Documentation/Website/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: "/autodeskmachinecontrolframework/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
