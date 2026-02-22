0ecc36f3ae8a84c06bc7c298055f80ed78cbecf3
Alexander Oster <alexanderoster@users.noreply.github.com>
2026-02-22 17:12:23 +0000

    Unregister legacy api/ui handler

--- Implementation/API/amc_api_factory.cpp
+++ Implementation/API/amc_api_factory.cpp
@@ -40,7 +40,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "API/amc_api_handler_build.hpp"
 #include "API/amc_api_handler_root.hpp"
 #include "API/amc_api_handler_frontend.hpp"
-#include "API/amc_api_handler_ui.hpp"
 #include "API/amc_api_handler_external.hpp"
 #include "API/amc_api_handler_auth.hpp"
 #include "API/amc_api_handler_executions.hpp"
@@ -73,7 +72,6 @@ CAPIFactory::CAPIFactory(PAPI pAPI, PSystemState pSystemState, std::vector <AMC:
 	pAPI->registerHandler(std::make_shared <CAPIHandler_Status>(MachineInstanceList, pSystemState->getStateMachineData (), pSystemState->getClientHash()));
 	pAPI->registerHandler(std::make_shared <CAPIHandler_Upload>(pSystemState));
 	pAPI->registerHandler(std::make_shared <CAPIHandler_Build>(pSystemState));
-	pAPI->registerHandler(std::make_shared <CAPIHandler_UI>(pSystemState));
 	pAPI->registerHandler(std::make_shared <CAPIHandler_Frontend>(pSystemState));
 	pAPI->registerHandler(std::make_shared <CAPIHandler_Version>(pSystemState));
 	pAPI->registerHandler(std::make_shared <CAPIHandler_External>(pSystemState));
